b07b418dd6f38c6c2aae91c09fd7f375ad085716
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-04-02 19:02:07 +0530

    CSS and JS minor fixes

--- js/faq.js
+++ js/faq.js
@@ -56,6 +56,33 @@ function handleFaq(e){
   }
 }
 
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+var items = document.querySelectorAll("li");
+
+function isItemInView(item){
+  var rect = item.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isItemInView(items[i])) {
+        items[i].classList.add("show");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
 
 
 
